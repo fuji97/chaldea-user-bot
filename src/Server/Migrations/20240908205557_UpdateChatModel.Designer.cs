3ec725629373828e4ad90aee1ddf09597e1a5eab
Federico Rapetti <fuji1097@gmail.com>
2024-09-09 23:03:14 +0200

    Update migrations

--- src/Server/Migrations/20240908205557_UpdateChatModel.Designer.cs
+++ src/Server/Migrations/20240908205557_UpdateChatModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Server.DbContext;
@@ -11,9 +12,11 @@ using Server.DbContext;
 namespace Server.Migrations
 {
     [DbContext(typeof(MasterContext))]
-    partial class MasterContextModelSnapshot : ModelSnapshot
+    [Migration("20240908205557_UpdateChatModel")]
+    partial class UpdateChatModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Server/Migrations/20240908205557_UpdateChatModel.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateChatModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "TelegramChats");
+
+            migrationBuilder.DropColumn(
+                name: "InviteLink",
+                table: "TelegramChats");
+
+            migrationBuilder.DropColumn(
+                name: "StickerSetName",
+                table: "TelegramChats");
+
+            migrationBuilder.RenameColumn(
+                name: "CanSetStickerSet",
+                table: "TelegramChats",
+                newName: "IsForum");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Newsletters",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsForum",
+                table: "TelegramChats",
+                newName: "CanSetStickerSet");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "TelegramChats",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InviteLink",
+                table: "TelegramChats",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StickerSetName",
+                table: "TelegramChats",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Newsletters",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
